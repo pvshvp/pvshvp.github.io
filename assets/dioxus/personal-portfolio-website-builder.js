0fd1f725ca47073404649becd2ca77c6161dc3d9
shiva-patt-work <shiva.patt.work@gmail.com>
2023-09-10 19:35:46 +0000

    Update from https://github.com/shiva-patt-work/personal-portfolio-website-builder/commit/6669b333445e25ff8418e6ddf18e001394b41264

--- assets/dioxus/personal-portfolio-website-builder.js
+++ assets/dioxus/personal-portfolio-website-builder.js
@@ -274,7 +274,7 @@ function __wbg_adapter_48(arg0, arg1, arg2) {
     try {
         _assertNum(arg0);
         _assertNum(arg1);
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7ab2a8f61f3e4256(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0c547faa685d898f(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
